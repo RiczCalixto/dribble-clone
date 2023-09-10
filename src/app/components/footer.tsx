db47ae37d262c47d88d5380977f365dc8afb743d
Ricardo Calixto <38529592+RiczCalixto@users.noreply.github.com>
2023-09-10 12:58:19 -0300

    feat: add navbar and footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { footerLinks } from "../constants";
+
+type FooterLink = { footerLink: { title: string; links: string[] } };
+const FooterColumn: React.FC<FooterLink> = ({ footerLink }) => (
+  <div className="footer_column">
+    <h4 className="font-semibold">{footerLink.title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {footerLink.links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexStart footer">
+      <div className="flex w-full flex-col gap-12">
+        <div className="flex flex-col items-start">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="Flexibble"
+          />
+          <p className="mt-5 max-w-xs text-start text-sm font-normal">
+            Flexible is the world&lsquo;s leading community for creatives to
+            share, grow and get hierd.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-12">
+          {footerLinks.map((footerLink) => (
+            <div className="flex flex-1 flex-col gap-4" key={footerLink.title}>
+              <FooterColumn footerLink={footerLink} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flexBetween footer_copyright">
+        <p>@2023 Flexibble. All rights reserved.</p>
+        <p className="text-gray">
+          <span className="font-semibold text-black">10.214</span> projects
+          submitted
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -1,21 +1,35 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { NavLinks } from "@/app/constants";
+import AuthProviders from "./auth-providers";
 
 const NavBar = () => {
+  let session;
   return (
-    <nav className="flexBetween gap-4 border-b border-nav-border px-8 py-5">
+    <nav className="flexBetween navbar">
       <div className="flexStart gap-10">
         <Link href="/">
           <Image src="/logo.svg" width={115} height={43} alt="Flexibble" />
         </Link>
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
+        <ul className="text-small hidden gap-7 xl:flex">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            <p>UseerPhoto</p>
+            <Link href={"/create-project"}> Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
       </div>
     </nav>
   );
